ddf8fe5b1bbf87b834dbae4d83f5dae2ef4b278d
dasl2001 <david.slivo2001@gmail.com>
2025-05-20 20:55:27 +0200

    fixed error

--- src/models/accomodation.js
+++ src/models/accomodation.js
@@ -1,6 +1,23 @@
-const sequelize = require('../db.js');
-const User = require('../models/user.js');
-const Accomodation = require('../models/accomodation.js');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+const User = require('./user');
+
+// Skapa modellen
+const Accomodation = sequelize.define('Accomodation', {
+  address:   { type: DataTypes.STRING,  allowNull: false },
+  city:      { type: DataTypes.STRING,  allowNull: false },
+  country:   { type: DataTypes.STRING,  allowNull: false },
+  zipcode:   { type: DataTypes.STRING,  allowNull: false }, // postnummer
+  rent:      { type: DataTypes.FLOAT,   allowNull: false }, // hyra
+  rooms:     { type: DataTypes.INTEGER, allowNull: false }  // antal rum
+});
+
+// User har många boenden, boende tillhör user (userId kopplas automatiskt)
+User.hasMany(Accomodation, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+Accomodation.belongsTo(User);
+
+module.exports = Accomodation;
+
 
 
 
--- tests/accomodation.test.js
+++ tests/accomodation.test.js
@@ -1,41 +1,20 @@
-const mongoose = require('mongoose');
-const User = require('../models/User');
-const Accommodation = require('../models/Accommodation');
+const sequelize = require('../src/db');
+const User = require('../src/models/user');
+const Accomodation = require('../src/models/accomodation');
 
-describe('Cascade delete', () => {
-  it('should delete accommodations when user is removed', async () => {
-    const user = await User.create({ name: 'Test User' });
-
-    await Accommodation.create({
-      address: 'Storgatan 1',
-      city: 'Göteborg',
-      country: 'Sverige',
-      postalCode: '41101',
-      rent: 9000,
-      rooms: 2,
-      userId: user._id
-    });
-
-    await user.remove();
-
-    const results = await Accommodation.find({ userId: user._id });
-    expect(results.length).toBe(0);
-  });
-});
-
+// Nollställer databasen före alla tester
 beforeAll(async () => { await sequelize.sync({ force: true }); });
 
 describe('Accomodation model', () => {
   test('cascade delete accomodation when user deleted', async () => {
+    // Skapa användare
+    const user = await User.create({ 
+      username: 'cascade', 
+      email: 'cascade@test.com', 
+      profileImage: 'http://img.com/c.jpg' 
+    });
 
-/*
-Skapar användare
-*/
-    const user = await User.create({ username: 'cascade', email: 'cascade@test.com', profileImage: 'http://img.com/c.jpg' });
-
-/*    
-Skapar boende kopplat till användaren
-*/    
+    // Skapa boende kopplat till användaren
     const accomodation = await Accomodation.create({
       address: 'Cascadegatan 2',
       city: 'Malmö',
@@ -43,18 +22,15 @@ Skapar boende kopplat till användaren
       zipcode: '22233',
       rent: 8000,
       rooms: 2,
-      UserId: user.id 
+      UserId: user.id // Kopplar ihop boendet med användaren
     });
 
-/*    
-Ta bort användaren 
-*/    
+    // Ta bort användaren (ska radera boendet automatiskt)
     await user.destroy();
 
-/*
-Kolla att boendet är borta
-*/
+    // Kontrollera att boendet är borta
     const found = await Accomodation.findByPk(accomodation.id);
     expect(found).toBeNull();
   });
 });
+
