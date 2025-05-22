d23f3e16b5308dcee1201baca47335d5dc6d8fd2
dasl2001 <david.slivo2001@gmail.com>
2025-05-22 18:26:49 +0200

    added postgresql

--- src/db.js
+++ src/db.js
@@ -1,32 +1,26 @@
 /*
 Importerar moduler
 */
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 /*
-Skapar en ny databasanslutning (Sequelize-instans)
-- Använder SQLite som databas
-- Lagrar all data i minnet (snabbt & temporärt, passar för tester)
-- Stänger av loggning (logging: false)
+Skapar en ny anslutning till databasen med hjälp av Sequelize
+Hämtar inställningar (databasnamn, användare, lösenord, host) från miljövariabler
+Om någon variabel saknas används 'postgres' och 'localhost' som standardvärden
 */
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: ':memory:',
-  logging: false
-});
-
-/*
-Slår PÅ foreign key constraints
-*/
-sequelize.authenticate().then(async () => {
-  if (sequelize.getDialect() === 'sqlite') {
-    await sequelize.query('PRAGMA foreign_keys = ON;');
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'postgres',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD || 'postgres',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: false,
   }
-});
+);
 
-/*
-Exporterar 
-*/
 module.exports = sequelize;
+
 
 
