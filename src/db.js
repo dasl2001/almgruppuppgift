fc66e794430624a38e37eb9d685d181810608fbd
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-27 14:54:11 +0200

    last commit

--- src/db.js
+++ src/db.js
@@ -1,32 +1,14 @@
-/*
-Importerar moduler
-*/
 const { Sequelize } = require('sequelize');
-
-/*
-Skapar en ny databasanslutning (Sequelize-instans)
-- Använder SQLite som databas
-- Lagrar all data i minnet (snabbt & temporärt, passar för tester)
-- Stänger av loggning (logging: false)
-*/
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: ':memory:',
   logging: false
 });
-
-/*
-Slår PÅ foreign key constraints
-*/
 sequelize.authenticate().then(async () => {
   if (sequelize.getDialect() === 'sqlite') {
     await sequelize.query('PRAGMA foreign_keys = ON;');
   }
 });
-
-/*
-Exporterar 
-*/
 module.exports = sequelize;
 
 
