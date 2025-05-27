7b639efdfd47b2b8e31a36223abf7101509a94cd
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-05-27 15:04:37 +0200

    last commit

--- src/app.js
+++ src/app.js
@@ -1,36 +1,12 @@
-/*
-Importerar moduler
-*/
 const express = require('express');
 const sequelize = require('./db.js');
 require('./models/user.js');
 require('./models/accomodation.js');
-
-/*
-Skapar Express-appen
-*/
 const app = express();
-
-/*
-Kan tolka JSON i inkommande requests (req.body)
-*/
 app.use(express.json());
-
 app.get('/', (req, res) => res.send('Welcome to the group of David Slivo and Kalle Soderberg'));
-
-/*
-Skapar tabellerna i databasen om de inte redan finns
-*/
 sequelize.sync(); 
-
-/*
-Exporterar appen
-*/
 module.exports = app;
-
-/*
-Om filen körs direkt (inte importeras) så startas en server på port 3000
-*/
 if (require.main === module) {
   app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
